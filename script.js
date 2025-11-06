2f9c39a27cefaa7766b678a5948e430d7dfec83c
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2025-11-06 16:42:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,127 @@
+// variables
+
+// var - global scope
+
+// let - block scope
+
+// const - block scope
+
+
+// var
+
+var a = 10   // declration // intialization
+
+a = 15       // reuse      // reintialiazation
+
+var a = 12   // redeclration
+
+console.log(a)
+
+
+// let 
+
+let b = 10         // declration   // intialiazation
+
+b = 14             // reuse        // reinti
+
+// let b = 15      // not accepted
+
+console.log(b);
+
+// const 
+
+const c = 15        // declration  // intialiazation
+
+// c = 22           // not accepted reUse, reInti
+
+// const c = 20     // not accept redeclration
+
+console.log(c);
+
+
+
+
+
+var x = 5;
+var x = 10;
+x = 15;
+console.log(x);  // 15
+
+
+let y = 8;
+y = 12;
+y = 20;
+console.log(y);  // error
+
+
+const z = 25;
+ 
+// const z = 40;
+console.log(z);   // error
+
+
+var num = 50;
+var num = 100;
+
+console.log(num); // 100
+
+
+
+let value = 10;
+
+ value = 20;
+
+console.log(value); // 20
+
+
+// printing statements
+
+// 1. console.log()
+
+
+console.log(100);
+
+
+// 2. alert()
+
+// alert("hello guys this is our first class , all the best")
+
+
+//  3 . confirm()  
+
+
+// confirm("did you accept my query")
+
+
+//  4.prompt()
+
+// prompt("what is your name :-")
+
+
+// 5. document.writeln()
+
+// document.writeln("hello")
+
+
+// i want to username :- 
+
+
+
+// let username = prompt("what is your name : -")
+
+// console.log(username);
+
+
+// i want show some message to user side
+
+
+// alert("hello")
+
+
+// i want to ask one question to user and i want user final wish
+
+// let userWish = confirm("did you ate")
+
+// console.log(userWish);
+
+
